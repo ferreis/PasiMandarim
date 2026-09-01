956787407197a82553956d2c17c4626125dc9b13
Rafael Fernando dos reis Mecabô <rafa_ferreis@hotmail.com>
2026-08-31 22:49:20 -0300

    fix(pages): corrige caminho base do GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/learning_Mandarin/' : '/',
+  base: command === 'build' ? '/PasiMandarim/' : '/',
   plugins: [vue()],
 }))
